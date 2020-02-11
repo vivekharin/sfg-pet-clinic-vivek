bd44192a2a1652eb5beffb876f71d4773268fbdb
Vivek Hari Narayanan <vivek.hari.n@gmail.com>
2020-02-11 20:58:51 +1100

    Initial Project Add

---
+++ src/main/java/vivek/guru/sfg/sfgpetclinicvivek/SfgPetClinicVivekApplication.java
@@ -0,0 +1,13 @@
+package vivek.guru.sfg.sfgpetclinicvivek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicVivekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicVivekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vivek/guru/sfg/sfgpetclinicvivek/SfgPetClinicVivekApplicationTests.java
@@ -0,0 +1,13 @@
+package vivek.guru.sfg.sfgpetclinicvivek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicVivekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
